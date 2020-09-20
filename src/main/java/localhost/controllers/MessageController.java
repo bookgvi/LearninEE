9789de4c24e1cd08d6f125d7796dd34c70e85b8b
bookgvi <bookgvi@gmail.com>
2020-09-20 10:52:10 +0300

    LearningEE: learning Request object

--- src/main/java/localhost/controllers/MessageController.java
+++ src/main/java/localhost/controllers/MessageController.java
@@ -3,13 +3,15 @@ package localhost.controllers;
 import services.getMessage.IMessage;
 
 import javax.inject.Inject;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 
-@WebServlet("/message")
+@WebServlet("/message/*")
 public class MessageController extends HttpServlet {
   @Inject
   private IMessage message;
@@ -21,10 +23,29 @@ public class MessageController extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String contextPath = request.getContextPath();
+    String servletPath = request.getServletPath();
+    String pathInfo = request.getPathInfo();
     String msg = "Hello, RESTfull";
     response.setContentType("application/json");
     response.setCharacterEncoding("UTF-8");
-    response.getWriter().write(String.valueOf(-2 >> 1));
+    response.getWriter().write(String.valueOf(-2 >> 1) + "\n");
+    response.getWriter().write(contextPath);
+    response.getWriter().write(servletPath);
+    response.getWriter().write(pathInfo + "\n");
+  }
+
+  @Override
+  protected void doPost (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    resp.getWriter().write(req.getContextPath() + "\n");
+    resp.getWriter().write(req.getServletPath() + "\n");
+    resp.getWriter().write(req.getPathInfo() + "\n");
+    resp.getWriter().write(req.getParameterMap() + "\n");
+    resp.getWriter().write(req.getParameter("name") + "\n");
+    resp.getWriter().write(req.getParameterNames() + "\n");
+    resp.getWriter().write(Arrays.toString(req.getParameterValues("note")) + "\n");
+    resp.getWriter().write(req.getAttributeNames() + "\n");
+    resp.getWriter().write(req.getHeader("content-type") + "\n");
   }
 
   protected synchronized void enterringServiceMethod() {}
--- src/main/java/localhost/controllers/MessageListener.java
+++ src/main/java/localhost/controllers/MessageListener.java
@@ -1,13 +1,17 @@
 package localhost.controllers;
 
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
+import javax.servlet.*;
 import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @WebListener()
-public class MessageListener implements ServletContextListener {
+public class MessageListener implements ServletContextListener, ServletContextAttributeListener,
+    HttpSessionListener, HttpSessionAttributeListener, ServletRequestListener, ServletRequestAttributeListener {
 
   private static final Logger log = Logger.getLogger("learningEE.web.messageServlet");
 
@@ -15,5 +19,10 @@ public class MessageListener implements ServletContextListener {
   public void contextInitialized(ServletContextEvent ctx) {
     log.log(Level.INFO, "QQQ", ctx.getServletContext());
     System.out.println("Context!!!!!!!!!!!!!!!!!");
+  }
+
+  @Override
+  public void sessionCreated (HttpSessionEvent se) {
+
   }
 }
